d87512fce708191d98f6adea62dddaf61c4ca824
Bruno <b.rezzio.93@gmail.com>
2019-02-06 12:48:52 -0300

    Se añade carpeta de diseños de pantallas

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -2,83 +2,91 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
-public class Jugador : MonoBehaviourPunCallbacks
+namespace Com.MyCompany.MyGame
 {
-    private Image avatar;
-    private string nombre;
-    private bool[] pago = new bool[10];
-    private int billetera;
-
-    [SerializeField]
-    private Text t_dias;
-
-    private int dias = 0;
-    private float probabilidad;
-
-    private int evasores = 0;
-
-    // Start is called before the first frame update
-    private void Start()
+    public class Jugador : MonoBehaviourPunCallbacks
     {
-        billetera = System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["monto"]);
-        t_dias.text = "Día " + System.Convert.ToString(dias + 1);
-    }
+        GameManager manager = new GameManager();
 
-    // Update is called once per frame
-    private void Update()
-    {
-    }
+        private Image avatar;
+        private string nombre;
+        private bool[] pago = new bool[10];
+        private int billetera;
 
-    /// <summary>
-    /// Revisa que el jugador pague su pasaje y en base a eso calcula la probabilidad de llegar junto con los
-    /// calculos del pasaje de bus
-    /// </summary>
-    /// <param name="button"></param>
-    public void OtroDia(bool button)
-    {
-        if (dias < 9)
+        [SerializeField]
+        private Text t_dias;
+
+        private int dias = 1;
+        private float probabilidad;
+
+        private int evasores = 0;
+
+        // Start is called before the first frame update
+        private void Start()
         {
-            Pagar(button);
-            Llegar();
-            dias++;
-            Debug.Log("Comienza Día " + (dias + 1));
-            Debug.Log("Saldo " + PhotonNetwork.LocalPlayer.NickName + ": " + billetera);
-            t_dias.text = "Día " + System.Convert.ToString(dias + 1);
+            billetera = System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["monto"]);
+            t_dias.text = "Día " + System.Convert.ToString(dias);
         }
-        else
+
+        // Update is called once per frame
+        private void Update()
         {
-            Debug.Log("Finalizado");
-            PhotonNetwork.LeaveRoom();            
-            SceneManager.LoadScene(0);
         }
-    }
 
-    /// <summary>
-    /// Se añade la selección del jugador a su historial de pago y se descuenta saldo de la billetera del jugador
-    /// </summary>
-    public void Pagar(bool button)
-    {
-        if (button) Debug.Log("Se pagó hoy");
-        else Debug.Log("No se pagó hoy");
-        //pago[dias] = button;
-        PhotonNetwork.LocalPlayer.CustomProperties["paga"] = button;
-        if (PhotonNetwork.LocalPlayer.CustomProperties["paga"].Equals(true))
-            billetera = billetera - System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["precio"]);
-    }
+        /// <summary>
+        /// Revisa que el jugador pague su pasaje y en base a eso calcula la probabilidad de llegar junto con los
+        /// calculos del pasaje de bus
+        /// </summary>
+        /// <param name="button"></param>
+        public void OtroDia(bool button)
+        {
+            if (dias <= 10)
+            {
+                Pagar(button);
+                Llegar();
+                Debug.Log(PhotonNetwork.LocalPlayer.CustomProperties["pago" + dias]);
+                dias++;
+                Debug.Log("Comienza Día " + (dias));                
+                Debug.Log("Saldo " + PhotonNetwork.LocalPlayer.NickName + ": " + billetera);
+                t_dias.text = "Día " + System.Convert.ToString(dias);
+                //manager.SwitchScenes(7);
+            }
+            else
+            {
+                for (int i = 1; i <= dias; i++)
+                    Debug.Log(PhotonNetwork.LocalPlayer.CustomProperties["pago"+i]);
+                Debug.Log("Finalizado");
+                PhotonNetwork.LeaveRoom();
+                SceneManager.LoadScene(0);
+            }
+        }
 
-    public void Llegar()
-    {
-        PhotonNetwork.LocalPlayer.CustomProperties["llega"] = CalcularViaje();
-        if (PhotonNetwork.LocalPlayer.CustomProperties["llega"].Equals(true))
-            billetera = billetera + System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["ganancia"]);
-    }
+        /// <summary>
+        /// Se añade la selección del jugador a su historial de pago y se descuenta saldo de la billetera del jugador
+        /// </summary>
+        public void Pagar(bool button)
+        {
+            if (button) Debug.Log("Se pagó hoy");
+            else Debug.Log("No se pagó hoy");
+            //pago[dias] = button;
+            PhotonNetwork.LocalPlayer.CustomProperties["pago"+dias] = button;
+            if (PhotonNetwork.LocalPlayer.CustomProperties["pago"+dias].Equals(true))
+                billetera = billetera - System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["precio"]);
+        }
 
-    /// <summary>
-    /// Calcula la probabilidad de llegar al destino en base a la cantidad de evasores totales
-    /// </summary>
-    public bool CalcularViaje()
-    {
-        return true;
+        public void Llegar()
+        {
+            PhotonNetwork.LocalPlayer.CustomProperties["llega"] = CalcularViaje();
+            if (PhotonNetwork.LocalPlayer.CustomProperties["llega"].Equals(true))
+                billetera = billetera + System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["ganancia"]);
+        }
+
+        /// <summary>
+        /// Calcula la probabilidad de llegar al destino en base a la cantidad de evasores totales
+        /// </summary>
+        public bool CalcularViaje()
+        {
+            return true;
+        }
     }
 }
--- Assets/Scripts/PlayerParameters.cs
+++ Assets/Scripts/PlayerParameters.cs
@@ -1,33 +1,41 @@
 ﻿using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 namespace Com.MyCompany.MyGame
 {
     public class PlayerParameters : MonoBehaviour
     {
-        public Image Avatar;        
+        public Image Avatar;
         public Image Chosen;
         public static string ChosenName;
-
-        private ExitGames.Client.Photon.Hashtable CustomProps = new ExitGames.Client.Photon.Hashtable();
 
         // Use this for initialization
         private void Start()
         {
         }
+
         private void Update()
         {
-            
         }
 
         public void SetPlayerProperties()
         {
             Debug.Log(ChosenName);
-            ExitGames.Client.Photon.Hashtable PlayerCustomProps = new ExitGames.Client.Photon.Hashtable();
-            CustomProps["paga"] = new bool[10];
-            CustomProps["llega"] = new bool[10];
+
+            ExitGames.Client.Photon.Hashtable pago = new ExitGames.Client.Photon.Hashtable();
+            ExitGames.Client.Photon.Hashtable llega = new ExitGames.Client.Photon.Hashtable();
+            for (int i = 1; i <= 10; i++)
+            {
+                pago.Add("pago"+i, false);
+                llega.Add("llega" + i, false);
+            }
+            PhotonNetwork.SetPlayerCustomProperties(pago);
+            PhotonNetwork.SetPlayerCustomProperties(llega);
+
+            ExitGames.Client.Photon.Hashtable CustomProps = new ExitGames.Client.Photon.Hashtable();
+            //CustomProps["paga"] = new bool[10];
+            //CustomProps["llega"] = new bool[10];
             CustomProps["Avatar"] = ChosenName;
             PhotonNetwork.SetPlayerCustomProperties(CustomProps);
         }
@@ -37,9 +45,7 @@ namespace Com.MyCompany.MyGame
             ChosenName = Avatar.sprite.name;
             Chosen.sprite = Avatar.sprite;
             Chosen.gameObject.SetActive(true);
-            Debug.Log("SetPlayerAvatar "+ChosenName);
+            Debug.Log("SetPlayerAvatar " + ChosenName);
         }
-
-
     }
 }
