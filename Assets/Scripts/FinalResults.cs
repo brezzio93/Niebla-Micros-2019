33c3a0916e91a2113634fb34703b7720ce2b46d0
Bruno <b.rezzio.93@gmail.com>
2019-02-08 17:04:39 -0300

    Se dejó funcionando la pantalla final, falta gráfico de lineas

--- Assets/Scripts/FinalResults.cs
+++ Assets/Scripts/FinalResults.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,20 +7,49 @@ namespace Com.MyCompany.MyGame
 {
     public class FinalResults : MonoBehaviour
     {
-
         [SerializeField]
         private Text nombreSala, pasajes, llegados;
 
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField]
+        private Image graphPagados, graphLlegados;
 
+        // Start is called before the first frame update
+        private void Start()
+        {
+            Llenar();
+
+            nombreSala.text = PhotonNetwork.CurrentRoom.Name;
+            pasajes.text = Contar("pago") + "/" + (PhotonNetwork.CurrentRoom.PlayerCount * 10);
+            llegados.text = Contar("llega") + "/" + (PhotonNetwork.CurrentRoom.PlayerCount * 10);
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
+        }
 
+        private void Llenar()
+        {
+            double lleno = System.Convert.ToDouble(Contar("pago"));
+            graphPagados.fillAmount = (float)lleno / (float)(PhotonNetwork.CurrentRoom.PlayerCount * 10);
+            lleno = System.Convert.ToDouble(Contar("llega"));
+            graphLlegados.fillAmount = (float)lleno / (float)(PhotonNetwork.CurrentRoom.PlayerCount * 10);
+        }
+
+        private string Contar(string str)
+        {
+            int cantidad = 0;
+            bool test;
+            for (int j = 1; j <= Jugador.dias; j++)
+            {
+                foreach (Player p in PhotonNetwork.PlayerList)
+                {
+                    test = System.Convert.ToBoolean(p.CustomProperties[str + j]);
+                    if (test) cantidad++;
+                }
+            }
+
+            return System.Convert.ToString(cantidad);
         }
     }
 }
