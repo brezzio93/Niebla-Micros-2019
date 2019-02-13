b33f684d478b5a2701fc385e1e97b411ea651b05
Bruno <b.rezzio.93@gmail.com>
2019-02-13 17:29:34 -0300

    Añadidos 7 gráficos predeterminados en escena 10

--- Assets/Scripts/SpriteControl.cs
+++ Assets/Scripts/SpriteControl.cs
@@ -12,14 +12,33 @@ namespace Com.MyCompany.MyGame
 
         private List<JugadoresEnEspera> buttons;
 
+        private string currentScene;
+
         // Start is called before the first frame update
         private void Start()
         {
             buttons = new List<JugadoresEnEspera>();
 
-            GameManager.instance.AlEntrarJugador += Instance_AlEntrarJugador;
+            currentScene = GameManager.instance.GetCurrentScene().name;
+            if (currentScene == "05 Espera")
+            {
+                GameManager.instance.AlEntrarJugador += Instance_AlEntrarJugador;
+                InstanciarBotones();
+            }
 
-            InstanciarBotones();
+            if (currentScene == "10 Resultados Finales")
+            {
+                GameManager.instance.AlEntrarJugador -= Instance_AlEntrarJugador;
+                ListarJugadores();
+            }
+        }
+
+        private void Update()
+        {
+            if (currentScene == "05 Espera")
+                ListarJugadoresEspera();
+            if (currentScene == "10 Resultados Finales")
+                ListarJugadores();
         }
 
         private void Instance_AlEntrarJugador(Player obj)
@@ -50,22 +69,57 @@ namespace Com.MyCompany.MyGame
 
         public void ListarJugadores()
         {
+            if (buttons.Count > 0)
+            {
+                foreach (JugadoresEnEspera button in buttons)
+                    Destroy(button.gameObject);
+            }
+            buttons.Clear();
+
+            foreach (Player p in PhotonNetwork.PlayerList)
+            {
+                JugadoresEnEspera button = Instantiate(playerTemplate) as JugadoresEnEspera;
+                button.gameObject.SetActive(true);
+
+                button.ConstruirAvatar(p.CustomProperties["Imagen"] as string);
+
+                button.transform.SetParent(playerTemplate.transform.parent, false);
+                buttons.Add(button);
+            }
+
+            foreach (JugadoresEnEspera button in buttons)
+            {
+                //foreach (Player p in PlayerList)
+                //{
+                //    Sprite sprite = GameManager.instance.Avatars.FirstOrDefault(s => s.name == (p.CustomProperties["Imagen"] as string));
+                //}
+            }
+        }
+
+        public void ListarJugadoresEspera()
+        {
             Player[] playerNoHost = PhotonNetwork.PlayerList;
-
             List<Player> PlayerList = new List<Player>();
-
-            for (int i = 0; i < playerNoHost.Length; i++)
-
-                PlayerList.Add(playerNoHost[i]);
-
+            PlayerList.AddRange(playerNoHost);
             PlayerList.Remove(PhotonNetwork.MasterClient);
 
-            //if (buttons.Count > 0)
-            //{
-            //    foreach (JugadoresEnEspera button in buttons)
-            //        Destroy(button.gameObject);
-            //}
-            //buttons.Clear();
+            if (buttons.Count > 0)
+            {
+                foreach (JugadoresEnEspera button in buttons)
+                    Destroy(button.gameObject);
+            }
+            buttons.Clear();
+
+            foreach (Player p in PlayerList)
+            {
+                JugadoresEnEspera button = Instantiate(playerTemplate) as JugadoresEnEspera;
+                button.gameObject.SetActive(true);
+
+                button.ConstruirAvatar(p.CustomProperties["Imagen"] as string);
+
+                button.transform.SetParent(playerTemplate.transform.parent, false);
+                buttons.Add(button);
+            }
 
             foreach (JugadoresEnEspera button in buttons)
             {
